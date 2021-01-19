b91bc020ae2f978e89b5eca4fa35b75ff12f511e
zuozhu.meng <zuozhu.meng@yitu-inc.com>
2021-01-19 14:58:52 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoClass.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * @author mengzz
+ **/
+public class DemoClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/example/demo/DemoClassTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author mengzz
+ **/
+class DemoClassTest {
+
+    @Test
+    void test() {
+    }
+}
