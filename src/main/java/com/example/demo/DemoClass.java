546896135fe2d17136c901614fcacfc8d8c29c4f
zuozhu.meng <zuozhu.meng@yitu-inc.com>
2021-01-23 15:34:21 +0800

    add findAny

--- src/main/java/com/example/demo/DemoClass.java
+++ src/main/java/com/example/demo/DemoClass.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author mengzz
  **/
 public class DemoClass {
     public static void main(String[] args) {
+        String str = "1";
+        List<String> list = Stream.of(str).collect(Collectors.toList());
+        String[] array = {str};
 
     }
 }
